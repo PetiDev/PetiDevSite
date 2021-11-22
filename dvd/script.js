3475b59fefd2bcfe5c52654df469a51bc05b99f7
ICreeper12 <54474482+ICreeper12@users.noreply.github.com>
2021-11-22 21:45:11 +0100

    design change

--- dvd/script.js
+++ dvd/script.js
@@ -1,9 +1,16 @@
 const obj = document.getElementById("obj");
 let px = 0;
 let py = 0;
-let vx = 2;
-let vy = 2;
+let vx = 1;
+let vy = 1;
 
+function rndNum(min, max) {
+    return Math.round(Math.random()*(max - min) + min)
+}
+function changeClr() {
+    return obj.style.color = `hsl(${rndNum(0, 360)},${rndNum(50, 100)}%,${rndNum(35, 100)}%)`
+}
+changeClr()
 
 setInterval(() => {
     py += vy;
@@ -12,10 +19,11 @@ setInterval(() => {
     obj.style.left = `${px}px`;
     if (px < 0 || px > window.innerWidth - obj.clientWidth) {
         vx = -vx
-    
+        changeClr()
     }
     if (py < 0 || py > window.innerHeight - obj.clientHeight) {
         vy = -vy
+        changeClr()
        }
 
 }, 1/60);
